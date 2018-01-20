72b28d5527a97112963efae28c7f42a3911f0b83
askeledzija <andrej.skeledzija@outlook.com>
2018-01-20 12:27:40 +0100

    Set Firefox and Chrome Headless mode

--- src/main/java/com/autotest/driver/LocalDriverFactory.java
+++ src/main/java/com/autotest/driver/LocalDriverFactory.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
@@ -23,7 +24,11 @@ public class LocalDriverFactory {
             System.setProperty("webdriver.gecko.driver", "geckodriver");
             System.setProperty("webdriver.firefox.logfile","Firefox.log" );
 
-            driver = new FirefoxDriver();
+            //Set Firefox Headless mode as TRUE
+            FirefoxOptions options = new FirefoxOptions();
+            //options.setHeadless(true);
+
+            driver = new FirefoxDriver(options);
             log.info("LocalDriverFactory created aa instance of WebDriver for: " + browserName);
             return driver;
         }
@@ -32,8 +37,11 @@ public class LocalDriverFactory {
             System.setProperty("webdriver.chrome.driver","chromedriver");
             System.setProperty("webdriver.chrome.logfile","Chrome.log" );
 
+
             ChromeOptions options = new ChromeOptions();
             options.addArguments("disable-infobars");
+            //Set Chrome Headless mode as TRUE
+            //options.setHeadless(true);
             //options.addArguments("--start-maximized");
             //options.addArguments("--kiosk");
 
